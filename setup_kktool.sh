ae4896f84e63854ad7d85b90584ed249f46e883a
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2024-12-07 23:23:52 +0700

    Add files via upload

--- setup_kktool.sh
+++ setup_kktool.sh
@@ -9,7 +9,8 @@ pkg install android-tools -y
 pip install pycurl
 pip install rich
 curl -o /sdcard/Download/kktool.py http://kktool.x10.bz/kktool.py
-echo "alias KKtool='python /sdcard/Download/kktool.py'" >> ~/.bashrc
+echo "alias KKtool='cd /sdcard/Download && python kktool.py'" >> ~/.bashrc
+
 source ~/.bashrc
 
 echo "Setup completed! You can now use the command 'KKtool' to run the script."
